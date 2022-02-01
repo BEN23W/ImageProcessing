93ea3e661856d574564254f8496dea341bc09785
BEN23W <ben511r@gmail.com>
2022-01-31 18:04:16 -0600

    Added grayscale functions

--- MedianFilter/main.cpp
+++ MedianFilter/main.cpp
@@ -4,12 +4,14 @@ int main(int argc, char* argv[]){
     Image test("test.jpg");
     test.write("new.png");
     Image copy = test;
+
     for(int i = 0; i<copy.width*copy.channels; ++i){
         copy.data[i] = 255;
     }
 
-
-
+    copy.write("copy.png");
+    Image blank(100,100,3);
+    blank.write("blank.jpg");
 
     return 0;
 }
---
+++ MedianFilter/median_filter.h
@@ -0,0 +1,17 @@
+#include<stdint.h>
+#include<cstdio>
+
+/* Noise Removal from Image using Median Filter
+**Median Filters are useful for salt and pepper noise and
+**preserving edges
+
+** Using a 3x3 sampling window */
+
+struct Median{
+
+    void InsertionSort(int arr, int n);
+    
+
+
+};
+
